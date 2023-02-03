c52bc8507bdb868b3cfcf48b5ddc3d31d67ca539
Dmlsito <101351451+Dmlsito@users.noreply.github.com>
2023-02-03 08:56:54 +0100

    sdf

---
+++ EjercicioHibernate/src/main/java/org/example/Suppliers.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Suppliers {
+
+    private int supplierId;
+    private String CompanyName;
+    private String ContactName;
+    private String ContactTitle;
+    private String Addres;
+    private String city;
+    private String region;
+    private String postalCode;
+    private String country;
+    private String phone;
+    private String fax;
+    private String homepage;
+
+    public Suppliers(){}
+
+
+
+
+}
